a085579ac40908b3b2eea98c0b42cef62fb8d000
Leandro Conca <lluccia@gmail.com>
2016-06-19 12:24:54 -0300

    Stored Procedure Dummy builder initial implementation.

---
+++ dbfit-java/oracle-mock-extension/src/main/java/dbfit/mock/StoredProcedureDummy.java
@@ -0,0 +1,119 @@
+package dbfit.mock;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import dbfit.util.Direction;
+import dbfit.util.OracleDbParameterAccessor;
+
+public class StoredProcedureDummy {
+
+    private String name;
+    private Map<String, OracleDbParameterAccessor> allParams = new HashMap<String, OracleDbParameterAccessor>();;
+
+    public StoredProcedureDummy(String name) {
+        this.name = name;
+    }
+
+    public void setAllParams(Map<String, OracleDbParameterAccessor> paramMap) {
+        this.allParams = paramMap;
+    }
+
+    public String buildDDL() {
+        return "CREATE OR REPLACE " +
+                    (isFunction() ? "FUNCTION" : "PROCEDURE") +
+                    " " + name.toUpperCase() +
+                    buildParameters() +
+                    " " + buildReturnTypeDeclaration() +
+                    "IS BEGIN " + 
+                    (isFunction() ? "RETURN " : "") +
+                    "NULL; " +
+                    "END;";
+    }
+
+    private String buildParameters() {
+        List<OracleDbParameterAccessor> orderedParameters = getOrderedParameters();
+        if (orderedParameters.isEmpty())
+            return "";
+        else {
+        	StringBuilder parametersDeclaration = new StringBuilder("(");
+        	
+        	int paramCount = 0;
+        	
+        	for (OracleDbParameterAccessor paramAccessor: orderedParameters) {
+        	    parametersDeclaration.append(paramAccessor.getName());
+        	    parametersDeclaration.append(" ");
+        	    
+        	    if (paramAccessor.hasDirection(Direction.OUTPUT))
+        	        parametersDeclaration.append("OUT ");
+        	    else if (paramAccessor.hasDirection(Direction.INPUT_OUTPUT))
+        	        parametersDeclaration.append("IN OUT ");
+        	    
+        	    parametersDeclaration.append(paramAccessor.getOriginalTypeName());
+        	    
+        	    if (++paramCount < orderedParameters.size())
+        	        parametersDeclaration.append(", ");
+        	}
+        	
+        	parametersDeclaration.append(")");
+        	
+            return parametersDeclaration.toString();
+        }
+    }
+    
+    private String buildReturnTypeDeclaration() {
+        if (isFunction())
+            return "RETURN " + getReturnValueType() + " ";
+        else
+            return "";
+    }
+    
+    private String getReturnValueType() {
+        if (isFunction())
+            return getReturnValue().getOriginalTypeName();
+        else
+            return null;
+    }
+
+    private List<OracleDbParameterAccessor> getOrderedParameters() {
+        List<OracleDbParameterAccessor> parameters = getParameters();
+        List<OracleDbParameterAccessor> orderedParameters = new ArrayList<OracleDbParameterAccessor>(parameters.size());
+        
+        orderedParameters.addAll(parameters);
+        
+        for (OracleDbParameterAccessor paramAccessor: parameters)
+            if(paramAccessor.doesNotHaveDirection(Direction.RETURN_VALUE))
+                orderedParameters.set(paramAccessor.getPosition(), paramAccessor);
+        
+        return orderedParameters;
+    }
+    
+    private List<OracleDbParameterAccessor> getParameters() {
+        List<OracleDbParameterAccessor> parameters = new ArrayList<OracleDbParameterAccessor>(allParams.size());
+        
+        for (OracleDbParameterAccessor paramAccessor: allParams.values())
+            if(paramAccessor.doesNotHaveDirection(Direction.RETURN_VALUE))
+                parameters.add(paramAccessor);
+        
+        return parameters;
+    }
+    
+    private OracleDbParameterAccessor getReturnValue() {
+        for (OracleDbParameterAccessor paramAccessor: allParams.values())
+            if(paramAccessor.hasDirection(Direction.RETURN_VALUE))
+                return paramAccessor;
+        
+        return null;
+    }
+    
+    private boolean isFunction() {
+        for (OracleDbParameterAccessor paramAccessor: allParams.values())
+            if(paramAccessor.hasDirection(Direction.RETURN_VALUE))
+                return true;
+        
+        return false;
+    }
+    
+}
---
+++ dbfit-java/oracle-mock-extension/src/test/java/dbfit/mock/StoredProcedureDummyTest.java
@@ -0,0 +1,72 @@
+package dbfit.mock;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import dbfit.util.Direction;
+import dbfit.util.OracleDbParameterAccessor;
+
+public class StoredProcedureDummyTest {
+	
+    @Test
+    public void canBuildProcedureDDL() {
+        StoredProcedureDummy spDummyProcName = new StoredProcedureDummy("procname");
+
+        assertThat(spDummyProcName.buildDDL(),
+                is("CREATE OR REPLACE PROCEDURE PROCNAME IS BEGIN NULL; END;"));
+
+        StoredProcedureDummy spDummyAnotherProcName = new StoredProcedureDummy("anotherprocname");
+        assertThat(spDummyAnotherProcName.buildDDL(),
+                is("CREATE OR REPLACE PROCEDURE ANOTHERPROCNAME IS BEGIN NULL; END;"));
+    }
+    
+    @Test
+    public void canBuildProcedureDDLWithParameters() {
+        StoredProcedureDummy spDummy = new StoredProcedureDummy("procname");
+
+        Map<String, OracleDbParameterAccessor> paramMap = new HashMap<String, OracleDbParameterAccessor>();
+        paramMap.put("param1", createParameter(0, "PARAM1", Direction.INPUT, "VARCHAR2"));
+        paramMap.put("param2", createParameter(1, "PARAM2", Direction.INPUT, "NUMBER"));
+        paramMap.put("param3", createParameter(2, "PARAM3", Direction.INPUT, "INTEGER"));
+        paramMap.put("param4", createParameter(3, "PARAM4", Direction.OUTPUT, "INTEGER"));
+        paramMap.put("param5", createParameter(4, "PARAM5", Direction.INPUT_OUTPUT, "INTEGER"));
+        
+        spDummy.setAllParams(paramMap);
+        
+        assertThat(spDummy.buildDDL(),
+                is("CREATE OR REPLACE PROCEDURE PROCNAME(PARAM1 VARCHAR2, PARAM2 NUMBER, PARAM3 INTEGER, PARAM4 OUT INTEGER, PARAM5 IN OUT INTEGER) IS BEGIN NULL; END;"));
+
+    }
+    
+    @Test
+    public void canBuildFunctionDDLWithParameters() {
+        StoredProcedureDummy spDummy = new StoredProcedureDummy("functionname");
+
+        Map<String, OracleDbParameterAccessor> paramMap = new HashMap<String, OracleDbParameterAccessor>();
+        paramMap.put("", createParameter(-1, "", Direction.RETURN_VALUE, "VARCHAR2"));
+        
+        spDummy.setAllParams(paramMap);
+        
+        assertThat(spDummy.buildDDL(),
+                is("CREATE OR REPLACE FUNCTION FUNCTIONNAME RETURN VARCHAR2 IS BEGIN RETURN NULL; END;"));
+
+    }
+
+	private OracleDbParameterAccessor createParameter(int position, String name, Direction direction, String type) {
+		return new OracleDbParameterAccessor(
+				name, 
+				direction,
+				0, 
+				null,
+				position,
+                null,
+                type, 
+                null);
+	}
+
+}
